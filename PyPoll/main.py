7647300345e27644dae794e7f18a66be83c47ad7
mattflanagan2 <matthewflanagan@Matthews-MBP.home>
2023-12-05 18:08:25 -0600

    main.py commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,23 @@
+import csv
+import os
 
+election_data_csv = os.path.join("/Users/matthewflanagan/Desktop/Data Analytics Work/Module 3/python-challenge/PyPoll/Resources/election_data.csv")
+
+total_votes = []
+winner = []
+candidates = {}
+candidates = dict()
+
+
+
+
+with open(election_data_csv) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        header = next(csvreader)
+
+        print(csvreader)
+        print(f"CSV Header: {header}")
+        for row in csvreader:
+                candidates = {"name": "Diana DeGette","Charles Casper Stockham","Raymon Anthony Doane"}
+                print(f'{candidates["name"]}')
+                
