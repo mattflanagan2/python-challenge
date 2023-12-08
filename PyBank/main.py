87536a355493551034c9a77a7dbebc8c1d2a2abc
Matt <146908072+mattflanagan2@users.noreply.github.com>
2023-12-07 18:27:26 -0600

    Update main.py
    with comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,38 +10,38 @@ total = 0
 previous_profit_or_losses = 0
 total_change = 0
 greatest_increase = 0
-greatest_increase_month = ""
+greatest_increase_month = ""            #defined as an empty string
 greatest_decrease = 0
 greatest_decrease_month = ""
 
-with open(budget_data_csv, 'r') as csvfile:
+with open(budget_data_csv, 'r') as csvfile:            #opening the csv file 
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
-    for row in csvreader:
-        date = row[0]
-        profit_or_losses = int(row[1])
-        total_months += 1
-        total += profit_or_losses
+    for row in csvreader:                #for each row in the csv reader
+        date = row[0]                    #assign the date variable to the first row
+        profit_or_losses = int(row[1])    #assign profit or loss variable as an integer (if you don't do this it won't recognize the negative values) and to the second row
+        total_months += 1                #count the total months (aka each row)
+        total += profit_or_losses        #add to the total for each profit or loss row 
         
-        if total_months > 1:
-            change = profit_or_losses - previous_profit_or_losses
-            total_change += change
-            if change > greatest_increase:
+        if total_months > 1:            #for each month after the first month
+            change = profit_or_losses - previous_profit_or_losses    #calculate the change
+            total_change += change                                    #add the change to the total change 
+            if change > greatest_increase:                            #if the change is greater than 0 or the previous number thereafter that becomes the new greatest change
                 greatest_increase = change
-                greatest_increase_month = date
-            if change < greatest_decrease:
+                greatest_increase_month = date                        #get the date for that greatest increase
+            if change < greatest_decrease:                            #same principal for the greatest decrease
                 greatest_decrease = change
                 greatest_decrease_month = date
 
         
-        previous_profit_or_losses = profit_or_losses
+        previous_profit_or_losses = profit_or_losses                #store previous row profit/loss value
         
-    average_change = total_change / (total_months-1)
+    average_change = total_change / (total_months-1)                #calculate average change
         
         
         
-    print("")
+    print("")                                                    #printing the results 
     print("Financial Analysis")
     print("------------------------------")
     print("")
@@ -52,8 +52,8 @@ with open(budget_data_csv, 'r') as csvfile:
     print(f'Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})')
     print("")
 
-    output_path = 'financial_analysis.txt'
-    with open(output_path, 'w') as output_file:
+    output_path = 'financial_analysis.txt'                    #output path assumes the budget_data_csv path from top
+    with open(output_path, 'w') as output_file:                #writing the text file to that path
         output_file.write("Financial Analysis\n")
         output_file.write("------------------------------\n")
         output_file.write(f'Total Months: {total_months}\n')
